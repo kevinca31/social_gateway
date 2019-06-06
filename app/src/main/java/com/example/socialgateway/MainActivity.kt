9c3a72c89fb8dd28180bbfbc44806c515c535d5f
Pascal <pascal.fuehrlich@student.hpi.de>
2019-06-06 17:37:16 +0200

    server requests

--- app/src/main/java/com/example/socialgateway/MainActivity.kt
+++ app/src/main/java/com/example/socialgateway/MainActivity.kt
@@ -1,14 +1,17 @@
 package com.example.socialgateway
 
 import android.app.AlertDialog
-import android.content.Intent
+import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import java.text.SimpleDateFormat
+import org.json.JSONObject
+import java.net.ConnectException
+import java.net.HttpURLConnection
+import java.net.URL
 import java.util.*
 
 data class SocialApp(
@@ -16,20 +19,22 @@ data class SocialApp(
     val packageName: String,
     val buttonId: Int)
 
-class MainActivity : AppCompatActivity() {
+fun openConnection(route: String): HttpURLConnection {
+    return URL("http://192.168.178.23:5000$route").openConnection() as HttpURLConnection
+}
 
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val userId = UUID.randomUUID().toString()  // TODO
+
+
         val socialApps = listOf(
-            SocialApp(resources.getString(R.string.whats_app),
-                "com.whatsapp",
-                R.id.whats_app_button),
-            SocialApp(resources.getString(R.string.telegram),
-                "org.telegram.messenger",
-                R.id.telegram_button))
+            SocialApp(resources.getString(R.string.whats_app), "com.whatsapp", R.id.whats_app_button),
+            SocialApp(resources.getString(R.string.telegram), "org.telegram.messenger", R.id.telegram_button))
         socialApps.forEach { socialApp ->
             val button = findViewById<Button>(socialApp.buttonId)
             button.setOnClickListener {
@@ -37,32 +42,64 @@ class MainActivity : AppCompatActivity() {
                 if (intent == null) {
                     val message = resources.getString(R.string.X_was_not_found_on_your_device, socialApp.name)
                     Toast.makeText(this, message, Toast.LENGTH_LONG).show()
-                } else {
-                    startActivityAfterQuestion(intent, socialApp.name)
+                    return@setOnClickListener
+                }
+
+                AsyncTask.execute {
+                    var question: String
+                    try {
+                        openConnection("/question").apply {
+                            if (responseCode != 200) {
+                                throw ConnectException("response code $responseCode")
+                            }
+
+                            question = inputStream.reader().readText()
+                            disconnect()
+                        }
+                    } catch (exception: ConnectException) {
+                        startActivity(intent)
+                        Log.d("aaaaaa", "could not request question: ${exception.message.orEmpty()}")
+                        return@execute
+                    }
+
+                    runOnUiThread {
+                        val answerEditText = EditText(this)
+                        AlertDialog.Builder(this).apply {
+                            setTitle(question)
+                            setView(answerEditText)
+                            setNegativeButton(android.R.string.cancel) { _, _ -> }
+                            setPositiveButton(android.R.string.ok) { _, _ ->
+                                AsyncTask.execute {
+                                    try {
+                                        openConnection("/answer").apply {
+                                            requestMethod = "POST"
+                                            doOutput = true
+                                            val answer = answerEditText.text.toString()
+                                            val answerJson = JSONObject("{" +
+                                                    "user_id: $userId," +
+                                                    "app_name: ${socialApp.name}," +
+                                                    "question: $question," +
+                                                    "answer: $answer," +
+                                                    "}")
+                                            outputStream.write(answerJson.toString().toByteArray())
+                                            if (responseCode != 200) {
+
+                                                throw ConnectException("response code $responseCode")
+                                            }
+                                            disconnect()
+                                        }
+                                    } catch (exception: ConnectException) {
+                                        Log.d("aaaaaa", "could not send answer: ${exception.message.orEmpty()}")
+                                    }
+                                }
+                                startActivity(intent)
+                            }
+                            create()
+                            show()
+                        }
+                    }
                 }
             }
         }
-    }
-
-    private fun startActivityAfterQuestion(intent: Intent, appName: String) {
-        val answer = EditText(this)
-        AlertDialog.Builder(this).apply {
-            val question = getQuestion(appName)
-            setTitle(question)
-            setView(answer)
-            setNegativeButton(android.R.string.cancel) { _, _ -> }
-            setPositiveButton(android.R.string.ok) { _, _ ->
-                val now = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US).format(Calendar.getInstance().time)
-                Log.d("answer", "started $appName ($now)")
-                Log.d("answer", "$question - ${answer.text}")
-                startActivity(intent)
-            }
-            create()
-            show()
-        }
-    }
-
-    private fun getQuestion(appName: String): String {
-        return "What do you expect of using $appName right now?"
     }
 }
